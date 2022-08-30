0b7610759d8972359da9a9cae84e23e503c7f6db
PedroMends30 <107001055+PedroMends30@users.noreply.github.com>
2022-08-30 11:19:10 -0300

    Add files via upload

---
+++ Metodos.java
@@ -0,0 +1,23 @@
+public class Metodos{
+    public static void Square(double num){
+        double quad = Math.pow(num,2);
+        System.out.println(quad);
+    }
+    public static void Maior3(int A, int B, int C){
+        int result = Math.max(Math.max(A,B),C);
+        System.out.println(result);
+    }
+    public static void Sexo(char sexo){
+        char Gender = Character.toUpperCase(sexo);
+        switch (Gender){
+            case 'M':
+                System.out.println("Masculino");
+                break;
+            case 'F':
+                System.out.println("Feminino");
+                break;
+            default:
+                System.out.println("Desconhecido");       
+        }
+    }
+}
