25300928da00ce10eb61bf1d95d9280fb8178e39
PedroMends30 <107001055+PedroMends30@users.noreply.github.com>
2022-10-14 21:13:55 -0300

    Up Bot
    upload do meu primeiro bot de automação web em Java.

---
+++ BotJava/opensite/src/test/java/tests/OpenSiteTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenSiteTest {
+
+	private WebDriver driver;
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ictus arte\\Desktop\\Pedro\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void test() throws InterruptedException{
+		driver.get("https://marcelodebittencourt.com/demopages/demosimplesearch/");
+		assertTrue("titulo difere",driver.getTitle().contentEquals("Demo Simple Search using JavaScript"));
+		Thread.sleep(3000);
+	}
+}
